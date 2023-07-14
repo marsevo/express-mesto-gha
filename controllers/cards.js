272fb2ea6c20446bfc0e97eb4fb68bb9104ad895
marsevo <sevost.rita@gmail.com>
2023-07-14 15:27:52 +0300

    fix: Правки после первой проверки (Можно лучше)

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,84 +1,73 @@
+const { ERROR_VALIDATION, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../errors/errors');
 const Card = require('../models/card');
 
-const { ERROR_VALIDATION, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../errors/errors');
+// Общая логика для обработки ошибок при выполнении операций с карточками
+const handleCardOperation = (req, res, operation) => {
+  const { cardId } = req.params;
+  operation(cardId)
+    .then((card) => {
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_VALIDATION).send({ message: 'Переданные данные некорректны' });
+      } else if (err.message === 'Not Found') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Карточка не найдена' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', err: err.message });
+      }
+    });
+};
 
+// Контроллер для получения всех карточек
 const getCards = (req, res) => {
   Card.find({})
     .populate('owner')
     .then((cards) => {
       res.send(cards);
     })
-    .catch(err => {
-      res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message });
-    })
+    .catch((err) => {
+      res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', err: err.message });
+    });
 };
 
+// Контроллер для создания новой карточки
 const createCard = (req, res) => {
   const ownerId = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner: ownerId })
-    .then(card => {
-      res.send(card)
+    .then((card) => {
+      res.send(card);
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
+        res.status(ERROR_VALIDATION).send({ message: 'Переданные данные некорректны' });
       } else {
-        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
+        res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', err: err.message });
       }
     });
 };
 
+// Контроллер для удаления карточки по ID
 const removeCardById = (req, res) => {
-  const { cardId } = req.params;
-  Card.findByIdAndRemove(cardId)
-  .orFail(() => new Error("Not Found"))
-  .then((card) => res.send(card))
-  .catch((err) => {
-    if (err.name === "CastError") {
-      res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
-      return;
-    } else if (err.name = "Not Found") {
-      res.status(ERROR_NOT_FOUND).send({ message: `Переданные данные некорректны` });
-      return;
-    } else {
-      res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message });
-    }
-  })
+  handleCardOperation(req, res, (cardId) =>
+  Card.findByIdAndRemove(cardId).orFail(() => new Error('Not Found')));
 };
 
+// Контроллер для установки лайка на карточке
 const putCardLike = (req, res) => {
-  const userId = req.user._id;
-  const { cardId } = req.params;
-  Card.findByIdAndUpdate(cardId, { $addToSet: { likes: userId } }, { new: true })
-    .orFail(() => new Error("Not Found"))
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === "CastError") {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
-      } else if (err.name = "Not Found") {
-        res.status(ERROR_NOT_FOUND).send({ message: `Переданные данные некорректны` });
-      } else {
-        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message });
-      }
-    })
+  handleCardOperation(req, res, (cardId) =>
+    Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .orFail(() => new Error('Not Found'))
+  );
 };
 
+// Контроллер для удаления лайка с карточки
 const removeCardLike = (req, res) => {
-  const userId = req.user._id;
-  const { cardId } = req.params;
-  Card.findByIdAndUpdate(cardId, { $pull: { likes: userId } }, { new: true })
-    .orFail(() => new Error("Not Found"))
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === "CastError") {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
-      } else if (err.name = "Not Found") {
-        res.status(ERROR_NOT_FOUND).send({ message: `Переданные данные некорректны` });
-      } else {
-        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message });
-      }
-    })
+  handleCardOperation(req, res, (cardId) =>
+    Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .orFail(() => new Error('Not Found'))
+  );
 };
 
 module.exports = {
@@ -86,5 +75,5 @@ module.exports = {
   createCard,
   removeCardById,
   putCardLike,
-  removeCardLike
+  removeCardLike,
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,89 +1,80 @@
-const User = require('../models/user')
-
 const { ERROR_VALIDATION, ERROR_NOT_FOUND, ERROR_DEFAULT } = require('../errors/errors');
+const User = require('../models/user');
 
+// Общая логика для обновления данных пользователя
+const updateUser = (req, res, updateData) => {
+  const userId = req.user._id;
+  User.findByIdAndUpdate(userId, updateData, { new: true, runValidators: true })
+    .then((user) => {
+      if (user) {
+        res.send(user);
+      } else {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь не найден' });
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_VALIDATION).send({ message: 'Переданные данные некорректны' });
+      } else {
+        res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', err: err.message });
+      }
+    });
+};
+
+// Контроллер для обновления профиля пользователя
+const updateUserProfile = (req, res) => {
+  const { name, about } = req.body;
+  updateUser(req, res, { name, about });
+};
+
+// Контроллер для обновления аватара пользователя
+const updateUserAvatar = (req, res) => {
+  const { avatar } = req.body;
+  updateUser(req, res, { avatar });
+};
+
+// Функция-декоратор для получения всех пользователей
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
       res.send(users);
     })
-    .catch(err => {
-      res.status(ERROR_DEFAULT).send({ message: `Произошла ошибка при получении пользователей`, err: err.message });
+    .catch((err) => {
+      res.status(ERROR_DEFAULT).send({ message: 'Произошла ошибка при получении пользователей', err: err.message });
     });
 };
 
+// Функция-декоратор для создания нового пользователя
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
-    .then(user => {
+    .then((user) => {
       res.status(201).send(user);
     })
-    .catch(err => {
+    .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
+        res.status(ERROR_VALIDATION).send({ message: 'Переданные данные некорректны' });
       } else {
-        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
+        res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', err: err.message });
       }
     });
 };
 
+// Функция-декоратор для получения пользователя по ID
 const getUserById = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
-    .orFail(() => new Error("Not Found"))
+    .orFail(() => new Error('Not Found'))
     .then((user) => res.send(user))
     .catch((err) => {
-      if (err.name === "CastError") {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
-      } if (err.message === "Not Found") {
-        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь не найден` });
+      if (err.name === 'CastError') {
+        res.status(ERROR_VALIDATION).send({ message: 'Переданные данные некорректны' });
+      } else if (err.message === 'Not Found') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь не найден' });
       }
-      res.status(ERROR_DEFAULT).sendsend({ message: `Произошла неизвестная ошибка`, err: err.message });
+      res.status(ERROR_DEFAULT).send({ message: 'Произошла неизвестная ошибка', err: err.message });
     });
 };
-
-const updateUserProfile = (req, res) => {
-  const { name, about } = req.body;
-  const userId = req.user._id; // Получаем _id пользователя из объекта req.user
-  User.findByIdAndUpdate(userId, { name, about }, { new: true, runValidators: true })
-    .then((user) => {
-      if (user) {
-        res.send(user);
-      } else {
-        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь не найден` });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
-      } else {
-        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
-      }
-    })
-};
-
-const updateUserAvatar = (req, res) => {
-  const { avatar } = req.body;
-  const userId = req.user._id; // Получаем _id пользователя из объекта req.user
-  User.findByIdAndUpdate(
-    userId,
-    { avatar },
-    { new: true, runValidators: true })
-    .then((user) => {
-      if (user) {
-        res.send(user);
-      } else {
-        res.status(ERROR_NOT_FOUND).send({ message: `Пользователь не найден` });
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ERROR_VALIDATION).send({ message: `Переданные данные некорректны` });
-      } else {
-        res.status(ERROR_DEFAULT).send({ message: `Произошла неизвестная ошибка`, err: err.message })
-      }
-    })
-}
 
 module.exports = {
   getUsers,
